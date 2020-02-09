a5b34116b01bd6eec5f5bca9aa297da934db96f9
Andrew Brainerd <drwb333@gmail.com>
2020-02-09 11:43:17 -0500

    Add titles to icons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,13 +15,13 @@ const Home = () => {
       </h1>
       <div className={styles.links}>
         <HomeLink path={'https://github.com/andrew-brainerd'}>
-          <GitHubIcon />
+          <GitHubIcon title={'My GitHub'} />
         </HomeLink>
         <HomeLink path={'https://www.linkedin.com/in/andrewbrainerd3/'}>
-          <LinkedInIcon />
+          <LinkedInIcon title={'My LinkedIn'} />
         </HomeLink>
         <HomeLink path={'mailto:drwb333@gmail.com'}>
-          <EmailIcon />
+          <EmailIcon title={'My Email'} />
         </HomeLink>
       </div>
     </div>
