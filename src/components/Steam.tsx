5a8ac7f0119a1b78c4d3230ef9e73e271289bd36
Andrew Brainerd <drwb333@gmail.com>
2023-03-25 14:10:13 -0400

    Add Steam game list

--- src/components/Steam.tsx
+++ src/components/Steam.tsx
@@ -4,12 +4,17 @@ import { OwnedGame } from 'types/steam';
 
 import styles from 'styles/components/Steam.module.scss';
 
-const buildImageUrl = (appId: number, hash: string) => `http://media.steampowered.com/steamcommunity/public/images/apps/${appId}/${hash}.jpg`;
+const buildImageUrl = (appId: number, hash: string) =>
+  `http://media.steampowered.com/steamcommunity/public/images/apps/${appId}/${hash}.jpg`;
 
-const Game = ({ appid, name, img_logo_url, img_icon_url }: OwnedGame) => (
+const Game = ({ appid, name, img_logo_url, img_icon_url, playtime_forever, rtime_last_played }: OwnedGame) => (
   <div key={appid} className={styles.game}>
-    <div><img src={buildImageUrl(appid, img_logo_url || img_icon_url)} /></div>
+    <div>
+      <img src={buildImageUrl(appid, img_logo_url || img_icon_url)} />
+    </div>
     <div>{name}</div>
+    {/* <div>{Math.round(playtime_forever / 60)}</div> */}
+    {/* <div>{rtime_last_played > 1676768400 ? 'PLAYED RECENTLY' : ''}</div> */}
   </div>
 );
 
@@ -30,9 +35,14 @@ const Steam = () => {
   return (
     <div>
       {games
-        .sort((a, b) => b.rtime_last_played - a.rtime_last_played)
-        .filter(game => game.playtime_forever > 0)
-        .map(game => <Game {...game} />)}
+        // .sort((a, b) => b.rtime_last_played - a.rtime_last_played)
+        // .sort((a, b) => b.playtime_forever - a.playtime_forever)
+        .sort((a, b) => b.playtime_forever - a.playtime_forever)
+        .filter(game => Math.round(game.playtime_forever / 60) > 10)
+        .filter(game => game.rtime_last_played > 1676768400)
+        .map(game => <Game {...game} />)
+        .slice(0, 10)
+      }
     </div>
   );
 };
